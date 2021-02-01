bc29856c3fa944f18e44cc803ec96f4bf8601f8a
Fatma Tanrisevdi <fatma@armut.com>
2021-02-02 01:00:52 +0300

    no message

--- rest/settings.py
+++ rest/settings.py
@@ -62,8 +62,7 @@ MIDDLEWARE = [
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
-    "https://swe573-client.herokuapp.com/"
+    "http://localhost:4200"
 ]
 
 ROOT_URLCONF = 'rest.urls'
