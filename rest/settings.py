6a01613c1a1565351fcbba0a724e0d7dd18b7b36
Fatma Tanrisevdi <fatma@armut.com>
2021-02-01 15:14:53 +0300

    Cors problemm fix

--- rest/settings.py
+++ rest/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "https://swe573-client.herokuapp.com/"
